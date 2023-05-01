13d1b1bdc04925ed9e54bc8834e38fd7bef17a71
Viktoria <vorobeva.vik.99@mail.ru>
2023-05-01 22:41:02 +0300

    добавляет пк блок абонементов

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,7 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {Form} from './modules/form-validate/form';
 import {breakpointChecker} from './modules/breakpoints';
+import {initTabs} from './modules/tabs/init-tabs';
 
 // ---------------------------------
 
@@ -18,6 +19,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
+    initTabs();
     const form = new Form();
     window.form = form;
     form.init();
