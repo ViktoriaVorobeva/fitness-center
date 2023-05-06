45d616536c0b5af8f079187e12046eff585b861e
Viktoria <vorobeva.vik.99@mail.ru>
2023-05-06 16:49:11 +0300

    добавляет блок тренеры

--- source/js/modules/slider.js
+++ source/js/modules/slider.js
@@ -1,25 +1,57 @@
+import Swiper from '../vendor/swiper';
+
 const sliderElement = document.querySelector('.trainers__slider');
+
+function addTabIndex() {
+  const slideElements = document.querySelectorAll('.trainers__slide:not(.swiper-slide-duplicate)');
+  for (let slide of slideElements) {
+    slide.querySelector('.card').setAttribute('tabindex', '0');
+  }
+}
 
 const initSlider = () => {
   if (sliderElement) {
-    const slider = new Swiper('.trainers__slider', {
+    const slider = new Swiper(sliderElement, {
       direction: 'horizontal',
       loop: true,
-      slidesPerView: 'auto',
+      slidesPerView: 1,
       spaceBetween: 30,
-      initialSlide: 2,
       centeredSlides: true,
+      initialSlide: 2,
       autoResize: false,
       loopedSlides: 0,
       maxBackfaceHiddenSlides: 0,
+
       keyboard: {
         enabled: true,
         onlyInViewport: true,
       },
 
+      breakpoints: {
+        768: {
+          spaceBetween: 30,
+          centeredSlides: false,
+          slidesPerView: 2,
+          initialSlide: 2,
+        },
+
+        1200: {
+          spaceBetween: 40,
+          initialSlide: 0,
+          centeredSlides: false,
+          slidesPerView: 4,
+        },
+      },
+
+      on: {
+        init: () => {
+          addTabIndex();
+        },
+      },
+
       navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '.trainers__slider-button--prev',
+        prevEl: '.trainers__slider-button--next',
       },
     });
   }
